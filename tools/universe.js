f98f74c4acc2b67aa4176a35fe1ad35666f39e14
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2019-11-23 12:06:29 +0530

    Code cleanup

--- tools/universe.js
+++ tools/universe.js
@@ -4,9 +4,9 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
 const readline = require('readline');
-const mappingmod =  require('./mappingmod.js');
+const mappingmod = require('./mappingmod.js');
 const partmod = require('./partmod.js');
-const seedrandom =  require('seedrandom');
+const seedrandom = require('seedrandom');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -32,20 +32,67 @@ function die(error) {
     process.exit(1);
 }
 
+function getRandomCoordinateUrl() {
+    const prg = new seedrandom()
+    const samplex = ((prg() * 2 - 1) * 10000).toFixed(0)
+    const sampley = ((prg() * 2 - 1) * 10000).toFixed(0);
+    return `https://threeuniverse.org/#x:${samplex}&z:${sampley}`
+
+}
 
 
-var gitorigin = ''
+function githubPageOfRepo(gitorigin) {
+    {
+        const githubcommonurlre = /https:\/\/github.com\/([A-Za-z0-9]+)\/threeuniverse.git/
+        const result = githubcommonurlre.exec(gitorigin)
+        if (result != null) {
+            const user = result[1];
+            return { pageurl: `https://${user}.github.io/threeuniverse/` }
+
+        }
+    }
+
+
+    {
+        const githubcommonurlre = /https:\/\/github.com\/threeuniverse\/([A-Za-z0-9]+)\/?/
+        const result = githubcommonurlre.exec(gitorigin)
+        if (result != null) {
+            const repo = result[1];
+            return { pageurl: `https://threeuniverse.github.io/${repo}/`, partnamehint: repo }
+        }
+
+    }
+
+
+    {
+
+        const githubcommonurlre = /https:\/\/github.com\/threeuniverse\/([A-Za-z0-9]+).git/
+        const result = githubcommonurlre.exec(gitorigin)
+        if (result != null) {
+            partnamehint = repo;
+            const repo = result[1];
+            return { githubPageUrl: `https://threeuniverse.github.io/${repo}/`: partnamehint: repo }
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
 const upstream = "https://github.com/Shakthi/threeuniverse.git";
 
 
 
 async function main() {
     const { stdout } = await exec('git config --get remote.origin.url');
-    gitorigin = stdout.trim()
+    let knownGitorigin = stdout.trim()
 
-    const answer = await ask(`Enter the git url you want to push the repo [${gitorigin}]:`);
-    if (answer != "")
-        gitorigin = answer;
+    let gitorigin = await ask(`Enter the git url you want to push the repo [${knownGitorigin}]:`, knownGitorigin);
 
     if (gitorigin == upstream)
         die(`Cannot setup repo for upstream url [${gitorigin}]`);
@@ -54,76 +101,53 @@ async function main() {
         console.log('Deleting CNAME file for non upstream repo')
         fs.unlinkSync('CNAME')
     }
-    
-    let partnamehint=null;
-    let githubPageUrl;
-    {
-        const githubcommonurlre = /https:\/\/github.com\/([A-Za-z0-9]+)\/threeuniverse.git/
-        const result = githubcommonurlre.exec(gitorigin)
-        if (result != null) {
-            const user = result[1];
-            githubPageUrl = `https://${user}.github.io/threeuniverse/`
 
-        } else {
-
-            const githubcommonurlre = /https:\/\/github.com\/threeuniverse\/([A-Za-z0-9]+)\/?/
-            const result = githubcommonurlre.exec(gitorigin)
-            if (result != null) {
-                const repo = result[1];
-                githubPageUrl = `https://threeuniverse.github.io/${repo}/`
-                partnamehint = repo;
-            } else {
-
-                const githubcommonurlre = /https:\/\/github.com\/threeuniverse\/([A-Za-z0-9]+).git/
-                const result = githubcommonurlre.exec(gitorigin)
-                if (result != null) {
-                    partnamehint = repo;
-                    const repo = result[1];
-                    githubPageUrl = `https://threeuniverse.github.io/${repo}/`
-                }
-
-            }
-
-        }
-
-    }
+    let partnamehint = null;
+    let { githubPageUrl, partnamehint } = githubPageOfRepo(gitorigin);
 
     githubPageUrl = await ask(`Url git hub page fot the repo [${githubPageUrl}]:`, githubPageUrl)
-    const partFileName = await ask(`Name of the partfile you want to setup [${partnamehint}]:`,partnamehint)
-    const finaljs = githubPageUrl+"src/universe_parts/"+partFileName+".js"
-    console.log("Final Url will be:",finaljs);
+    const partFileName = await ask(`Name of the partfile you want to setup [${partnamehint}]:`, partnamehint)
+    const finaljs = githubPageUrl + "src/universe_parts/" + partFileName + ".js"
+    console.log("Final Url will be:", finaljs);
+    console.log(`Creating part file(${partFileName})`);
 
-    partmod.createASperePart(partFileName);
-    
-    const xz={x:0,z:0}
-    {
-        const prg = new seedrandom()
-        const samplex = ((prg() * 2 - 1) * 10000).toFixed(0)
-        const sampley = ((prg() * 2 - 1) * 10000).toFixed(0);
-        const sampleurl = `https://threeuniverse.org/#x:${samplex}&z:${sampley}`
+    const samplecoordinateUrl = getRandomCoordinateUrl();
+    const coordinateUrl = await ask(`Enter the sample coordinate url you want to setup the base [${samplecoordinateUrl}]:`, samplecoordinateUrl)
 
-        const answer = await ask(`Enter the sample coordinate url you want to setup the base [${sampleurl}]:`)
-        const coordinateUrl = answer == "" ? sampleurl : answer;
-
-        try {
-            const xztemp = coordinateUrl.split('#')[1].split('&');
-            xz.x = xztemp[0].split('x:')[1];
-            xz.z = xztemp[1].split('z:')[1];
-        } catch (error) {
-            console.error(error)
-        }
-
-        let mapingdata = {
-            position: { x:xz.x,z:xz.z},
-            url: finaljs,
-        }
-
-        mappingmod.open();
-        mappingmod.setLocalPart(githubPageUrl);
-        mappingmod.appingNewPartViaMarker(mapingdata);
-        mappingmod.write();
-        console.log("Modfied maping file, run universepull to recive change")
+    try {
+        const xztemp = coordinateUrl.split('#')[1].split('&');
+        xz.x = xztemp[0].split('x:')[1];
+        xz.z = xztemp[1].split('z:')[1];
+    } catch (error) {
+        die("Invalid coordinates")
     }
+
+    
+
+    console.log("Generating new part...")
+    partmod.createASperePart(partFileName);
+
+    
+    console.log("Updatting maping file for new part...")
+    mappingmod.open();
+    mappingmod.setLocalPart(githubPageUrl);
+
+    let mapingdata = {
+        position: { x: xz.x, z: xz.z },
+        url: finaljs,
+    }
+
+    mappingmod.appingNewPartViaMarker(mapingdata);
+    mappingmod.write();
+
+    if(knownGitorigin!=gitorigin){
+        console.log("Setting gitorigin...")    
+    }
+    
+    console.log("Modfied maping file ")
+    const { stdout } = await exec(`git remote set-url origin ${gitorigin}`);
+
+
     rl.close();
 }
 main();
